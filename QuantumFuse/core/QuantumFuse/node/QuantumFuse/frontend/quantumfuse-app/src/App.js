acadd96a214347de7406a77b92f9020ef0dcbdd8
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-05-27 15:30:46 -0400

    Update App.js

--- QuantumFuse/core/QuantumFuse/node/QuantumFuse/frontend/quantumfuse-app/src/App.js
+++ QuantumFuse/core/QuantumFuse/node/QuantumFuse/frontend/quantumfuse-app/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useCallback } from 'react';
 import { w3cwebsocket as W3CWebSocket } from 'websocket';
 import { Line, Bar } from 'react-chartjs-2';
 import 'chart.js/auto';
-import logo from './QuantumFuse.png';
+import logo from './logo.svg';
 import './App.css';
 
 const client = new W3CWebSocket('wss://api.quantumfuse.com/v2/realtime');
@@ -84,8 +84,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="Quantum Fuse Coin Logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>QuantumFuse Explorer</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
       <main>
         <div>
