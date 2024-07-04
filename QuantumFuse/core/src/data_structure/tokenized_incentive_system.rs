bbe8f7613df8b3c3517e22f30ef8284c528c4e4a
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-07-03 20:16:59 -0400

    Create tokenized_incentive_system.rs

---
+++ QuantumFuse/core/src/data_structure/tokenized_incentive_system.rs
@@ -0,0 +1,21 @@
+pub struct IncentiveSystem {
+    tokens: HashMap<String, SustainabilityToken>,
+}
+
+impl IncentiveSystem {
+    pub fn new() -> Self {
+        IncentiveSystem {
+            tokens: HashMap::new(),
+        }
+    }
+
+    pub fn reward(&mut self, owner: String, amount: f64) {
+        let token_id = format!("token_{}", self.tokens.len() + 1);
+        let token = SustainabilityToken::new(token_id.clone(), owner.clone(), amount);
+        self.tokens.insert(token_id, token);
+    }
+
+    pub fn get_token(&self, token_id: &str) -> Option<&SustainabilityToken> {
+        self.tokens.get(token_id)
+    }
+}
