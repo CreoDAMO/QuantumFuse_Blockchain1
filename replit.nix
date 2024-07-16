f8c91cff0fb97abca3577b1f3b0c7c69dfd47b00
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-07-16 15:28:16 -0400

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,41 +1,9 @@
-{ pkgs }: 
-let 
-  rust = pkgs.rust-bin.stable.latest;
-  go = pkgs.go_1_20;
-  python = pkgs.python3.withPackages (ps: with ps; [ 
-    pip 
-    pytest 
-    # Add other Python dependencies here if needed 
-  ]);
-  nodejs = pkgs.nodejs-18_x;
-in pkgs.mkShell {
-  buildInputs = [
-    rust
-    go
-    python
-    nodejs
+{ pkgs }: {
+  deps = [
     pkgs.cmake
+    pkgs.clang
+    pkgs.gcc
     pkgs.pkg-config
     pkgs.openssl
-    pkgs.libtool
-    pkgs.libsecp256k1
   ];
-
-  shellHook = ''
-    export PATH=$PATH:$HOME/.cargo/bin
-    export PATH=$PATH:$HOME/go/bin
-
-    # Install Rust components
-    rustup toolchain install nightly
-    rustup default nightly
-    rustup component add rust-src --toolchain nightly
-
-    # Install Python dependencies
-    pip install -r QuantumFuse/api/requirements.txt
-
-    # Install Node.js dependencies
-    cd QuantumFuse/frontend/quantumfuse-app && npm install
-
-    echo "Development environment is ready."
-  '';
 }
