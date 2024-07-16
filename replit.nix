2bc0ea3931d78d9fb31fd238997942ad3eec5424
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-07-16 17:31:55 -0400

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,9 +1,27 @@
 { pkgs }: {
   deps = [
+    pkgs.glibcLocales
+    pkgs.tk
+    pkgs.tcl
+    pkgs.qhull
+    pkgs.gtk3
+    pkgs.gobject-introspection
+    pkgs.ghostscript
+    pkgs.freetype
+    pkgs.ffmpeg-full
+    pkgs.cairo
+    pkgs.kubo
     pkgs.cmake
     pkgs.clang
     pkgs.gcc
     pkgs.pkg-config
     pkgs.openssl
+    pkgs.rustup
+    pkgs.rustc
+    pkgs.cargo
+    pkgs.libopenssl
+    pkgs.zlib
+    pkgs.libssh2
+    pkgs.protobuf
   ];
 }
