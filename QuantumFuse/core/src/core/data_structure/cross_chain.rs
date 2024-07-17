007f822d29ba6e1d3a0223ebc08113fa5785df70
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-07-17 01:42:41 -0400

    Create cross_chain.rs

---
+++ QuantumFuse/core/src/core/data_structure/cross_chain.rs
@@ -0,0 +1,9 @@
+pub fn connect_to_substrate_node() -> Result<(), Box<dyn std::error::Error>> {
+    // Implementation
+    Ok(())
+}
+
+pub fn submit_extrinsic() -> Result<(), Box<dyn std::error::Error>> {
+    // Implementation
+    Ok(())
+}
