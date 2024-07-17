2c472651e93c228694f3a494880d9667e5754169
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-07-17 01:51:23 -0400

    Create dao.rs

---
+++ QuantumFuse/core/src/core/data_structure/dao.rs
@@ -0,0 +1,11 @@
+pub fn create_dao(name: &str) -> Result<Dao, Box<dyn std::error::Error>> {
+    // Implementation
+    Ok(Dao {})
+}
+
+pub fn participate_in_dao(dao: &Dao, proposal_id: usize, vote: bool) -> Result<(), Box<dyn std::error::Error>> {
+    // Implementation
+    Ok(())
+}
+
+pub struct Dao {}
