edd64de028f89acca00da2bb55827128df594cf7
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-07-17 01:37:39 -0400

    Create data_policy.rs

---
+++ QuantumFuse/core/src/core/data_structure/data_policy.rs
@@ -0,0 +1,11 @@
+pub struct DataRetentionPolicy {
+    // Fields
+}
+
+pub struct PrivacyPolicy {
+    // Fields
+}
+
+pub fn set_data_policies(retention_policy: DataRetentionPolicy, privacy_policy: PrivacyPolicy) {
+    // Implementation
+}
