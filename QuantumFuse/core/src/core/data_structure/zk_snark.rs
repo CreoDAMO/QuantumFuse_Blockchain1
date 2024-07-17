f8dde28837e213e3976ac3e61f82ffcf93fea14b
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-07-17 01:30:59 -0400

    Create zk_snark.rs

---
+++ QuantumFuse/core/src/core/data_structure/zk_snark.rs
@@ -0,0 +1,25 @@
+pub struct Prover {}
+
+impl Prover {
+    pub fn new() -> Self {
+        Prover {}
+    }
+
+    pub fn prove(&self, data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        // Implementation
+        Ok(vec![])
+    }
+}
+
+pub struct Verifier {}
+
+impl Verifier {
+    pub fn new() -> Self {
+        Verifier {}
+    }
+
+    pub fn verify(&self, proof: &[u8], data: &[u8]) -> Result<bool, Box<dyn std::error::Error>> {
+        // Implementation
+        Ok(true)
+    }
+}
