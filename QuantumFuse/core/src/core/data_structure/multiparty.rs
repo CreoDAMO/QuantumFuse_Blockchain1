547c7829a42bde1e9b129aff5ec35851fe576192
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-07-17 01:31:54 -0400

    Create multiparty.rs

---
+++ QuantumFuse/core/src/core/data_structure/multiparty.rs
@@ -0,0 +1,9 @@
+pub fn backup_keys(secret_key: &[u8], num_shares: usize, threshold: usize) -> Result<Vec<Vec<u8>>, Box<dyn std::error::Error>> {
+    // Implementation
+    Ok(vec![])
+}
+
+pub fn recover_keys(shares: Vec<Vec<u8>>) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    // Implementation
+    Ok(vec![])
+}
