6ebca886de136536c3235acbe1991b89a4071a89
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-07-17 01:47:54 -0400

    Create social_recovery.rs

---
+++ QuantumFuse/core/src/core/data_structure/social_recovery.rs
@@ -0,0 +1,9 @@
+pub fn distribute_key(secret_key: &[u8], num_shares: usize, threshold: usize) -> Result<Vec<Vec<u8>>, Box<dyn std::error::Error>> {
+    // Implementation
+    Ok(vec![])
+}
+
+pub fn recover_key(shares: Vec<Vec<u8>>) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+    // Implementation
+    Ok(vec![])
+}
