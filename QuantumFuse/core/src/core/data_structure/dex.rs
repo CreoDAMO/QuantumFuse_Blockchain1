8ae1589f4d0e92ab1319ad727c927aa797de7826
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-07-17 01:45:35 -0400

    Create dex.rs

---
+++ QuantumFuse/core/src/core/data_structure/dex.rs
@@ -0,0 +1,9 @@
+pub fn place_order() -> Result<(), Box<dyn std::error::Error>> {
+    // Implementation
+    Ok(())
+}
+
+pub fn get_order_book() -> Result<String, Box<dyn std::error::Error>> {
+    // Implementation
+    Ok("Order book".to_string())
+}
