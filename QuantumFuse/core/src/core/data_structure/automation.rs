2f5ee3a35925a2a1852337ec990d1d7413a204e2
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-07-17 01:44:18 -0400

    Create automation.rs

---
+++ QuantumFuse/core/src/core/data_structure/automation.rs
@@ -0,0 +1,9 @@
+pub fn automate_staking() -> Result<(), Box<dyn std::error::Error>> {
+    // Implementation
+    Ok(())
+}
+
+pub fn automate_yield_farming() -> Result<(), Box<dyn std::error::Error>> {
+    // Implementation
+    Ok(())
+}
