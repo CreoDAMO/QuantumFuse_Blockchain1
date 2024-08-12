11a13ce22a8edc4b4f52a74bceaf7c43738cd2a9
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-08-12 16:46:46 -0400

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -6,9 +6,7 @@
 
   shellHook = ''
     export PATH=$HOME/.cargo/bin:$PATH
-    if ! rustup show | grep -q "nightly"; then
-      rustup install nightly
-    fi
+    rustup toolchain install nightly
     rustup default nightly
     rustup component add rust-src
   '';
