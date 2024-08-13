104775ac9ca00b282e08b721a57a27f6cf7ddf05
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-08-13 14:07:46 -0400

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -2,12 +2,33 @@
   deps = [
     pkgs.cargo
     pkgs.rustup
+    pkgs.go
+    pkgs.python3
+    pkgs.nodejs
+    pkgs.protobuf
+    pkgs.python3Packages.pip
+    pkgs.python3Packages.virtualenv
   ];
 
   shellHook = ''
+    # Rust setup
     export PATH=$HOME/.cargo/bin:$PATH
     rustup toolchain install nightly
     rustup default nightly
     rustup component add rust-src
+
+    # Go setup
+    export GOPATH=$HOME/go
+    export PATH=$GOPATH/bin:$PATH
+    mkdir -p $GOPATH
+
+    # Python setup
+    python3 -m venv venv
+    source venv/bin/activate
+    pip install -r QuantumFuse/api/requirements.txt
+
+    # Node.js setup
+    cd QuantumFuse/frontend/quantumfuse-app
+    npm install
   '';
 }
