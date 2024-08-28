116f2a6968f667a374f5ed35d5d425439bcca0fb
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-08-27 20:23:15 -0400

    Update main.rs

--- QuantumFuse/core/src/main.rs
+++ QuantumFuse/core/src/main.rs
@@ -1,9 +1,3 @@
-mod core; // your core modules
-mod wallet;
-
-use core::*;
-use wallet::*;
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Main application logic here
