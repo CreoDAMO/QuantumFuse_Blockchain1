fe549bdfe4489eb03715efc4b6401e0f9d222daa
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-09-03 19:34:24 -0400

    Create counter.rs

---
+++ QuantumFuse/core/src/core/web_ui/counter.rs
@@ -0,0 +1,20 @@
+use wasm_react::{h, Component, VNode};
+use wasm_react::hooks::use_state;
+
+struct Counter {
+    initial_counter: i32,
+}
+
+impl Component for Counter {
+    fn render(&self) -> VNode {
+        let counter = use_state(|| self.initial_counter);
+
+        h!(div).build((
+            h!(p).build(("Counter: ", *counter.value())),
+            h!(button).on_click(&Callback::new({
+                clones!(mut counter);
+                move |_| counter.set(|c| c + 1)
+            })).build("Increment"),
+        ))
+    }
+  }
