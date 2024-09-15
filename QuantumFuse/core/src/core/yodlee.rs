12f24d56e5f2ff594e194c34693a9a1093659b45
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-09-15 16:11:17 -0400

    Update yodlee.rs

--- QuantumFuse/core/src/core/yodlee.rs
+++ QuantumFuse/core/src/core/yodlee.rs
@@ -1,3 +1,4 @@
+// src/core/yodlee.rs
 pub fn get_yodlee_account_balances(client_id: &str, secret: &str, access_token: &str) -> Result<(), Box<dyn std::error::Error>> {
     // Implementation
     Ok(())
