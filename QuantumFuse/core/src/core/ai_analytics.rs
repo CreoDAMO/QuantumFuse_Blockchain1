88e7b2e48bdf70994a956a6f39134745c150fe5a
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2024-09-15 15:39:17 -0400

    Update ai_analytics.rs

--- QuantumFuse/core/src/core/ai_analytics.rs
+++ QuantumFuse/core/src/core/ai_analytics.rs
@@ -1,5 +1,4 @@
-use orx_split_vec::SplitVec;  // Import SplitVec
-
+// src/core/ai_analytics.rs
 pub fn get_predictive_recommendations(portfolio: &str) -> Result<SplitVec<Recommendation>, Box<dyn std::error::Error>> {
     Ok(SplitVec::new())  // Use SplitVec instead of vec
 }
